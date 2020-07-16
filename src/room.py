55c5621a6686d0a09a895c7c7744044c991ae7f4
davorin <47928470+davorinpiljic@users.noreply.github.com>
2020-07-15 20:31:13 -0600

    player and room added

--- src/room.py
+++ src/room.py
@@ -1,2 +1,7 @@
 # Implement a class to hold room information. This should have name and
 # description attributes.
+
+class Room:
+    def __init__(self, name, current_room):
+        self.current_room = current_room
+        self.name = name
